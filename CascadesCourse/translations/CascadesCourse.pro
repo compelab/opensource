3a06f36b7cfa5bd24e484833af84e0d004a15ea9
Dielson Sales <dielsonscarvalho@gmail.com>
2014-11-25 13:39:32 -0300

    Sample project for Cascades Course

---
+++ CascadesCourse/translations/CascadesCourse.pro
@@ -0,0 +1,1 @@
+include (../CascadesCourse.pro)
---
+++ CascadesCourse/translations/CascadesCourse.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="22"/>
+        <location filename="../assets/main.qml" line="25"/>
+        <source>Filmes em cartaz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="46"/>
+        <location filename="../assets/main.qml" line="49"/>
+        <location filename="../assets/main.qml" line="52"/>
+        <source>Cinemas</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
